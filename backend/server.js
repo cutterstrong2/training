64e94db6db7c2ac172211bcf6f614d38a373df33
Randy Strong <rstrong2893@gmail.com>
2021-01-14 08:58:33 -0600

    make it better

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,19 @@ app.get('*', (req, res) =>
 //   res.send('Server is ready');
 // });
 
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+//Serve static assets if in production
+if (process.env.NODE_ENV ==='production'){
+  //set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*',(req,res)=> {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
